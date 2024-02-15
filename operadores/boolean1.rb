5d870911ac0dcaeaa4805753d611675dca8eb7ff
programadorpro@proton.me <programadorpro@proton.me>
2024-02-15 20:45:44 -0300

    NEW commit

--- operadores/boolean1.rb
+++ operadores/boolean1.rb
@@ -1,15 +1,21 @@
-puts "digite um número inteiro"
+
+puts "digite um numero inteiro "
 a = gets.chomp .to_i
 
-puts "digte outro número inteiro"
-
+puts "digite outro numero inteiro"
 b = gets.chomp .to_i
 
-puts "#{a} é igual que #{b} ? #{a == b}"
-puts "#{a} e diferente que ?#{b} #{a != b}"
-puts "#{a} é menor que  ?#{b} #{a < b}"
-puts "#{a} e maior que ?#{b} #{a > b}"
-puts "#{a} e menor ou igual que ?#{b} #{a <= b} "
-puts "#{a} e maior ou igual que ? #{b} #{a >= b }"
+puts "#{a} é igual que #{b}?#{a == b}"
+# == igualdade a == b resulta true se for igual a b , resulta flase caso contrario
+puts "#{a} e diferente que ? #{b} #{a != b}"
+# != diferente a !=b resulta true se a for diferente de b, resulta false caso contrário
+puts "#{a} é menor que ? #{b} #{a < b}"
+# menor que a < b resulta true se a for maior ou igual a b , resulta false caso contrário
+puts "#{a} e maior que ? #{b} #{a>b}"
+#  maior que a > b  resulta true se a for menor que b, resulta false caso contrário
+puts "#{a} e menor ou igual que ? #{b} #{a<=b}"
+#  menor ou igual a <= b resulta true se a for menor igual a b, resulta false caso contrario
+puts "#{a}e maior ou igual que ? #{b} #{a>=b}"
+#maior ou igual a >= b resulta true se a for menor ou igual a b , resulta false caso contrário
 
-
+# <=> resulta 0 se a for igual a b, retorna 1 se a for maior que b e retonra -1 se a for menor que b NOTA: SEMPRE RETORNA UM VALOR INTEIRO
