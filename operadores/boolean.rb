c1513b458a1b4bbfe36736169ae5abb695b8baaa
programadorpro@proton.me <programadorpro@proton.me>
2024-02-15 20:25:16 -0300

    aprendendo sobre booleanos

---
+++ operadores/boolean.rb
@@ -0,0 +1,10 @@
+a = 3
+b = 6
+
+puts a == b
+puts a != b
+puts a > b
+puts a < b 
+puts a >= b
+puts a <= b
+puts a <=> b
---
+++ operadores/boolean1.rb
@@ -0,0 +1,15 @@
+puts "digite um número inteiro"
+a = gets.chomp .to_i
+
+puts "digte outro número inteiro"
+
+b = gets.chomp .to_i
+
+puts "#{a} é igual que #{b} ? #{a == b}"
+puts "#{a} e diferente que ?#{b} #{a != b}"
+puts "#{a} é menor que  ?#{b} #{a < b}"
+puts "#{a} e maior que ?#{b} #{a > b}"
+puts "#{a} e menor ou igual que ?#{b} #{a <= b} "
+puts "#{a} e maior ou igual que ? #{b} #{a >= b }"
+
+
