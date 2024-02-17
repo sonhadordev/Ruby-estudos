03c7b2939f2a77722f1611e76160a5e94e13d14c
programadorpro@proton.me <programadorpro@proton.me>
2024-02-17 15:42:05 -0300

    case

---
+++ estruturas/case1.rb
@@ -0,0 +1,19 @@
+puts "digite o valor do imc"
+    imc = gets.chomp.to_i
+
+    case imc
+
+    when 0..20
+        puts "muito abaixo do peso"
+        
+    when 21..40
+puts "abaixo do peso"
+
+    when 41..60
+puts "acima do peso"
+
+    when 61..80
+puts "obesso"
+    when 81..100
+        puts "muito obesso"
+    end
